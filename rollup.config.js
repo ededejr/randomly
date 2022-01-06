433583b7b44fa68e77b89d23f3cd5ce483ef4037
Edede Oiwoh <eoiwoh@gmail.com>
2022-01-06 12:25:49 -0500

    chore: Configure rollup for minified builds

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import analyze from 'rollup-plugin-analyzer';
+import { terser } from 'rollup-plugin-terser';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+  input: 'build/index.js',
+  output: [
+    {
+      file: 'build/index.js',
+      format: 'cjs',
+      name: 'randomly',
+    }
+  ],
+  plugins: [
+    terser({
+      // prevents class names from being minified
+      keep_classnames: true,
+      // prevents function names from being minified
+      keep_fnames: true
+    }),
+    analyze({ 
+      summaryOnly: true 
+    })
+  ]
+};
+
+export default config;
